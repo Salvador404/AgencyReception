e132d563ce9d2de7f7925145bf5472bea4092bfd
Salvador404 <yasermhmdoost@gmail.com>
2025-05-18 20:26:56 +0330

    Infrastructures added

--- reception/main.cpp
+++ reception/main.cpp
@@ -2,8 +2,57 @@
 
 using namespace std;
 
+
+class custom_Node
+{
+	public:
+		int vip;
+		int enter;
+		custom_Node *next=nullptr;
+		custom_Node(int,int);
+};
+custom_Node::custom_Node(int v , int e)
+{
+	vip=v;
+	enter=e;
+}
+
+
+class eventList
+{
+	private:
+		custom_Node *head=nullptr;
+	public:
+		void append(int,int );
+};
+
+///////////-------------------------/////////////
+void eventList:: append(int v,int e )
+{
+	if(!head){ head=new custom_Node(v,e);}
+	else{
+		custom_Node *ptr=head;
+		while(ptr->next){ptr=ptr->next;}
+		ptr->next=new custom_Node(v,e);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    
     return 0;
 }
